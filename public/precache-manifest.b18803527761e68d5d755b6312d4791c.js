77dcac127895ecb16f774245a095812d098d0bab
jgimenoperez <jgimenoperez@gmail.com>
2021-06-03 18:01:29 +0200

    V1 Calencar app

---
+++ public/precache-manifest.b18803527761e68d5d755b6312d4791c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6d5a0b36eb1516f3e6912f129867b39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7f6b25ad091c774bdfa",
+    "url": "/static/css/2.b1ca7953.chunk.css"
+  },
+  {
+    "revision": "247caad394c892e048fe",
+    "url": "/static/css/main.f035ba6c.chunk.css"
+  },
+  {
+    "revision": "c7f6b25ad091c774bdfa",
+    "url": "/static/js/2.0dc74110.chunk.js"
+  },
+  {
+    "revision": "5061a6b3c66013452abf9ea07a5c3863",
+    "url": "/static/js/2.0dc74110.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "247caad394c892e048fe",
+    "url": "/static/js/main.969b38d2.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
